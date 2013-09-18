a82c1bfa1b9bba56fe7428a2b177070d20dd0363
EC2 Default User <ec2-user@ip-10-160-143-81.us-west-1.compute.internal>
2013-09-18 10:49:22 +0000

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Manorson::Application.config.secret_key_base = 'b7f0cec7d625fdf12641563b87441d0f571810560c3c220f4ac285e892e7b785a736a9e0efa2391ec406776ce56ecbac3626e16f3ad112e0a07a2e8fbf64ba6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Manorson::Application.config.session_store :cookie_store, key: '_manorson_session'
